d171615ae71241a41a6872131d45676dc10a21d0
Dmitriy Shmaliuk <dimkas2001@icloud.com>
2020-05-12 01:13:42 +0300

    add (constants.js): ENGLISH_ALPHABET

--- constants.js
+++ constants.js
@@ -77,3 +77,32 @@ export const CODING_SYMBOLS = [
     '9',
     ' '
 ];
+
+export const ENGLISH_ALPHABET = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+];
